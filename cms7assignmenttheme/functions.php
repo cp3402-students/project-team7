7fcb8e630f8a9236495d786484bc9578e6fc188f
amber.hogarth <amber.hogarth@my.jcu.edu.au>
2024-04-10 23:31:56 +1000

    Finalise theme

--- cms7assignmenttheme/functions.php
+++ cms7assignmenttheme/functions.php
@@ -121,17 +121,17 @@ add_action( 'after_setup_theme', 'u3a_team7_theme_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function u3a_team7_theme_widgets_init() {
-    register_sidebar(
-        array(
-            'name'          => esc_html__( 'Footer', 'u3a-team7-theme' ),
-            'id'            => 'footer-widgets',
-            'description'   => esc_html__( 'Add widgets here to display in the footer.', 'u3a-team7-theme' ),
-            'before_widget' => '<div id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h2 class="widget-title">',
-            'after_title'   => '</h2>',
-        )
-    );
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'u3a-team7-theme' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'u3a-team7-theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'u3a_team7_theme_widgets_init' );
 
--- cms7assignmenttheme/index.php
+++ cms7assignmenttheme/index.php
@@ -52,5 +52,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-// get_sidebar();
+get_sidebar();
 get_footer();
